5e0d1c4785ea5008e73d813de88d12275b298374
Liviu112 <liviu.nastase2@mail.dcu.ie>
2021-04-16 17:13:56 +0100

    product
    sevlet

--- src/productsServlet.java
+++ src/productsServlet.java
@@ -104,5 +104,63 @@ public class productsServlet extends HttpServlet {
 		  System.out.println("Product added! ");
 		  return "success";
 	}
+	
+	protected String modifyStockProducts(HttpServletRequest request) {
+		// set connection info	
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		con = connectionInfo.connectToDB();
+		int ProductID =-1;
+		
+		// get the values from the form
+		String ScientificName = "ScientificName"; //request.getParameter("pwd")
+		String CommonName = "CommonName";
+		String Dimension = "Dimension";
+		String Colour = "Colour";
+		double Price = 50.123;
+		String Maturity = "Maturity";
+		int Availability = 100;
+		
+
+		  try {
+			  System.out.println("\nConnection Successful..... creating statement....");
+		     	 stmt = con.createStatement();
+			     rs = stmt.executeQuery("SELECT * FROM thegardencenterdatabase.productsinformation");
+
+			    	// find the last product in the table
+				     while (rs.next())
+				     {  
+				    	 System.out.println("the " + rs.getInt("ProductID"));
+				    	 ProductID = rs.getInt("ProductID");
+				     }
+				
+				ProductID++;
+				// insert the new user 
+				PreparedStatement pstmt = con.prepareStatement("INSERT INTO thegardencenterdatabase.productsinformation "
+						+ "(ProductID, ScientificName, CommonName, Dimension, Colour, Price, Maturity, Availability, LastEntryRefresh) VALUES (?,?,?,?,?,?,?,?,?)");
+					pstmt.clearParameters();       // Clears any previous parameters
+					pstmt.setInt(1, ProductID);
+					pstmt.setString(2, ScientificName);
+					pstmt.setString(3, CommonName);
+					pstmt.setString(4, Dimension);
+					pstmt.setString(5, Colour);
+					pstmt.setDouble(6, Price);
+					pstmt.setString(7, Maturity);
+					pstmt.setInt(8, Availability);
+					pstmt.setString(9, Date.getTime());
+					pstmt.executeUpdate();
+		  }
+		        catch (SQLException e) {
+			     System.out.println("\nAn error has occurred during the Statement/ResultSet phase.  Please check the syntax and study the Exception details!");
+		            while (e != null) {
+			         System.out.println(e.getMessage());
+		                e = e.getNextException();
+			     }
+		            System.exit(0);
+		        }
+		  System.out.println("Product added! ");
+		  return "success";
+	}
 
 }
